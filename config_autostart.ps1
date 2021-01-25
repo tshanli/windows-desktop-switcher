0771fe8019040ac38574413be1696eea0b326d59
Tshan Li <tshanli@outlook.com>
2021-01-25 14:43:51 +0100

    feat: configure out the program at autostart

---
+++ config_autostart.ps1
@@ -0,0 +1,9 @@
+# Run the following commands in an Administrator powershell prompt.
+# Be sure to specify the correct path to your desktop_switcher.ahk file.
+
+$A = New-ScheduledTaskAction -Execute "PATH\TO\desktop_switcher.ahk"
+$T = New-ScheduledTaskTrigger -AtLogon
+$P = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators" -RunLevel Highest
+$S = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit 0
+$D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+Register-ScheduledTask WindowsDesktopSwitcher -InputObject $D:w
